12a82b3864b6b9f20a213bab6d8bef9ab6299ae1
Sofistication <sordonez93@gmail.com>
2017-03-27 14:44:20 -0400

    Add model test
    - check that retrieve_data returns a `Tempfile`

--- spec/models/dataset_spec.rb
+++ spec/models/dataset_spec.rb
@@ -3,5 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe Dataset, type: :model do
-  pending 'add some examples to (or delete) #{__FILE__}'
+  def dataset_params
+    {
+      name: 'Property Assessment',
+      year: 2017,
+      resource: '9a4b1173-89ac-4a01-93e7-661eeb81ba16'
+    }
+  end
+
+  before(:all) do
+    Dataset.create!(dataset_params)
+  end
+
+  after(:all) do
+    Dataset.delete_all
+  end
+
+  describe 'retrieve_data' do
+    it 'returns an object of type Tempfile' do
+      expect(Dataset.first.retrieve_data.class).to eq(Tempfile)
+    end
+  end
 end
