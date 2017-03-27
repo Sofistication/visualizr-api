c70fa260568ec47d56b8d49b9bcca18f880f633d
Sofistication <sordonez93@gmail.com>
2017-03-27 17:19:43 -0400

    Process data from boston API
    - created method `process_data` to return processed data
    - processed data = count of number of entries per zipcode
    
    TODO: implement pagination (will probably require caching)
    instead of only looking at first page of data

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -11,4 +11,29 @@ class Dataset < ApplicationRecord
     buffer = open(url)
     buffer
   end
+
+  # def next_page(url)
+  #   full_url = "https://data.boston.gov#{url}"
+  #   buffer = open(full_url)
+  #   buffer
+  # end
+
+  def strip_data(data_array)
+    zipcode_count = {}
+    data_array.each do |row|
+      zipcode = row['ZIPCODE']
+      if zipcode_count[zipcode]
+        zipcode_count[zipcode] += 1
+      else
+        zipcode_count[zipcode] = 1
+      end
+    end
+    zipcode_count
+  end
+
+  def process_data
+    response = JSON.parse(retrieve_data.read)
+    return Nil unless response['success']
+    strip_data response['result']['records']
+  end
 end
