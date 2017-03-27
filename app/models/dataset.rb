bb7f61ac02f97e168e1f261c33acae74fbbe6e90
Sofistication <sordonez93@gmail.com>
2017-03-26 21:25:52 -0400

    Retrieve data from Boston API
    - added method to return url for querying boston's api
    - added method to retrieve data from said api
    - `retrieve_data` returns a `File` object
    
    TODO: Process data on server and return to client.

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+require 'open-uri'
+
 class Dataset < ApplicationRecord
+  def boston_dataset
+    'https://data.boston.gov/api/action/datastore_search?resource_id='
+  end
+
+  def retrieve_data
+    url = "#{boston_dataset}#{resource}"
+    buffer = open(url)
+    buffer
+  end
 end
