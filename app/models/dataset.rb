d2f0205844e91cd5aab098dd448b3ec1e3e62ff1
Sofistication <sordonez93@gmail.com>
2017-04-25 14:30:08 -0400

    First attempt at processing all results

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -7,6 +7,10 @@ class Dataset < ApplicationRecord
 
   validates :name, :year, :resource, presence: true
 
+  def zipcode_count
+    {}
+  end
+
   def boston_dataset
     'https://data.boston.gov/api/action/datastore_search?resource_id='
   end
@@ -17,14 +21,13 @@ class Dataset < ApplicationRecord
     buffer
   end
 
-  # def next_page(url)
-  #   full_url = "https://data.boston.gov#{url}"
-  #   buffer = open(full_url)
-  #   buffer
-  # end
+  def paginate(url)
+    full_url = "https://data.boston.gov#{url}"
+    buffer = open(full_url)
+    JSON.parse(buffer.read)
+  end
 
-  def strip_data(data_array)
-    zipcode_count = {}
+  def strip_data(data_array, next_url)
     data_array.each do |row|
       zipcode = row['ZIPCODE']
       if zipcode_count[zipcode]
@@ -33,12 +36,15 @@ class Dataset < ApplicationRecord
         zipcode_count[zipcode] = 1
       end
     end
-    zipcode_count
+    response = paginate next_url
+    strip_data(response['result']['records'], response['result']['_links']['next'])
   end
 
   def process_data
     response = JSON.parse(retrieve_data.read)
     return Nil unless response['success']
-    strip_data response['result']['records']
+    records = response['result']['records']
+    next_url = response['result']['_links']['next']
+    strip_data(records, next_url)
   end
 end
